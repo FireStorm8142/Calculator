c4829abe09fe9325a8e929ee3289f10e6985366e
Johan Paul <johanjohn405@gmail.com>
2025-12-22 16:36:33 +0530

    Initial commit...

---
+++ calculator.js
@@ -0,0 +1,92 @@
+let runnningTotal = 0;
+let buffer = "0";
+let previousOp = null;
+const screen = document.querySelector('.screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+    }
+    else{
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+            buffer ='0'
+            runnningTotal=0;
+            break;
+        case '=':
+            if(previousOp === null){
+                return
+            }
+            flushOperation(parseInt(buffer));
+            previousOp = null;
+            buffer = runnningTotal;
+            runnningTotal=0;
+            break;
+        case '←':
+            if(buffer.length===1){
+                buffer='0';
+            }
+            else{
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case '÷':
+        case '+':
+        case '−':
+        case '×':{
+            handleMath(symbol);
+            break;
+        }
+    }
+}
+function handleMath(symbol){
+    if (buffer ==='0'){
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+
+    if (runnningTotal === 0){
+        runnningTotal = intBuffer;
+    }
+    else{
+        flushOperation(intBuffer);
+    }
+    previousOp=symbol;
+    buffer='0';
+}
+function flushOperation(intBuffer){
+    if(previousOp === '+'){
+        runnningTotal += intBuffer;
+    }
+    else if(previousOp === '−'){
+        runnningTotal -= intBuffer;
+    }
+    else if(previousOp === '×'){
+        runnningTotal *= intBuffer;
+    }
+    else if(previousOp === '÷'){
+        runnningTotal /= intBuffer;
+    }
+}
+function handleNumber(numberString){
+    if(buffer === '0'){
+        buffer = numberString;
+    }
+    else{
+        buffer += numberString;
+    }
+}
+
+function init() {
+    document.querySelectorAll('button').forEach(button => {
+        button.addEventListener('click', event => {
+            buttonClick(event.target.innerText);
+        });
+    });
+}
+init();
